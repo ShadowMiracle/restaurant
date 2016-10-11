aac1b42fef871af6b9d9c0874c9bd03f38099562
Nguyen Hoang Tu <nguyenhoangtu91@gmail.com>
2016-10-11 16:25:11 +0700

    Add menu page [50%]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,37 +16,37 @@
 //= require bootstrap
 //= require jquery.slick
 //= require_tree .
-/** 
+/**
   * Template Name: SpicyX
-  * Version: 1.1  
+  * Version: 1.1
   * Template Scripts
   * Author: MarkUps
   * Author URI: http://www.markups.io/
 
   Custom JS
-  
 
-  1. FIXED NAVBAR 
+
+  1. FIXED NAVBAR
   2. TOP SLIDER
-  3. ABOUT US ( SLICK SLIDER ) 
+  3. ABOUT US ( SLICK SLIDER )
   4. DATEPICKER
   5. SHEF SLIDER ( SLICK SLIDER )
   6. TESTIMONIAL SLIDER ( SLICK SLIDER )
   7. COUNTER
-  8. LIGHTBOX ( FOR PORTFOLIO POPUP VIEW ) 
+  8. LIGHTBOX ( FOR PORTFOLIO POPUP VIEW )
   9. MENU SMOOTH SCROLLING
   10. HOVER DROPDOWN MENU
   11. SCROLL TOP BUTTON
-  12. PRELOADER  
+  12. PRELOADER
 
-  
+
 **/
 
 jQuery(function($) {
 
 
   /* ----------------------------------------------------------- */
-  /*  1. FIXED NAVBAR 
+  /*  1. FIXED NAVBAR
   /* ----------------------------------------------------------- */
 
 
@@ -98,54 +98,54 @@ jQuery(function($) {
   /*  5. SHEF SLIDER (SLICK SLIDER)
   /* ----------------------------------------------------------- */
 
-  jQuery('.mu-chef-nav').slick({
-    dots: true,
-    arrows: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      }, {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      }, {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
+  // jQuery('.mu-chef-nav').slick({
+  //   dots: true,
+  //   arrows: false,
+  //   infinite: true,
+  //   speed: 300,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 2,
+  //   autoplay: true,
+  //   autoplaySpeed: 2500,
+  //   responsive: [{
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 3,
+  //         infinite: true,
+  //         dots: true
+  //       }
+  //     }, {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2
+  //       }
+  //     }, {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1
+  //       }
+  //     }
+  //     // You can unslick at a given breakpoint now by adding:
+  //     // settings: "unslick"
+  //     // instead of a settings object
+  //   ]
+  // });
 
   /* ----------------------------------------------------------- */
   /*  6. TESTIMONIAL SLIDER (SLICK SLIDER)
   /* ----------------------------------------------------------- */
 
-  jQuery('.mu-testimonial-slider').slick({
-    dots: true,
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    speed: 500,
-    cssEase: 'linear'
-  });
+  // jQuery('.mu-testimonial-slider').slick({
+  //   dots: true,
+  //   infinite: true,
+  //   arrows: false,
+  //   autoplay: true,
+  //   speed: 500,
+  //   cssEase: 'linear'
+  // });
 
   /* ----------------------------------------------------------- */
   /*  7. COUNTER
@@ -157,12 +157,12 @@ jQuery(function($) {
   // });
 
   /* ----------------------------------------------------------- */
-  /*  8. LIGHTBOX ( FOR PORTFOLIO POPUP VIEW ) 
+  /*  8. LIGHTBOX ( FOR PORTFOLIO POPUP VIEW )
   /* ----------------------------------------------------------- */
 
   $('body').append("<div id='portfolio-popup'><div class='portfolio-popup-area'><div class='portfolio-popup-inner'></div></div></div>");
 
-  // WHEN CLICK PLAY BUTTON 
+  // WHEN CLICK PLAY BUTTON
 
   jQuery('.mu-view-btn').on('click', function(event) {
     event.preventDefault();
@@ -193,52 +193,52 @@ jQuery(function($) {
   //MENU SCROLLING WITH ACTIVE ITEM SELECTED
 
   // Cache selectors
-  var lastId,
-    topMenu = $(".mu-main-nav"),
-    topMenuHeight = topMenu.outerHeight() + 13,
-    // All list items
-    menuItems = topMenu.find('a[href^=\\#]'),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function() {
-      var item = $($(this).attr("href"));
-      if (item.length) {
-        return item; }
-    });
+  // var lastId,
+  //   topMenu = $(".mu-main-nav"),
+  //   topMenuHeight = topMenu.outerHeight() + 13,
+  //   // All list items
+  //   menuItems = topMenu.find('a[href^=\\#]'),
+  //   // Anchors corresponding to menu items
+  //   scrollItems = menuItems.map(function() {
+  //     var item = $($(this).attr("href"));
+  //     if (item.length) {
+  //       return item; }
+  //   });
 
-  // Bind click handler to menu items
-  // so we can get a fancy scroll animation
-  menuItems.click(function(e) {
-    var href = $(this).attr("href"),
-      offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 32;
-    jQuery('html, body').stop().animate({
-      scrollTop: offsetTop
-    }, 1500);
-    jQuery('.navbar-collapse').removeClass('in');
-    e.preventDefault();
-  });
+  // // Bind click handler to menu items
+  // // so we can get a fancy scroll animation
+  // menuItems.click(function(e) {
+  //   var href = $(this).attr("href"),
+  //     offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 32;
+  //   jQuery('html, body').stop().animate({
+  //     scrollTop: offsetTop
+  //   }, 1500);
+  //   jQuery('.navbar-collapse').removeClass('in');
+  //   e.preventDefault();
+  // });
 
-  // Bind to scroll
-  jQuery(window).scroll(function() {
-    // Get container scroll position
-    var fromTop = $(this).scrollTop() + topMenuHeight;
+  // // Bind to scroll
+  // jQuery(window).scroll(function() {
+  //   // Get container scroll position
+  //   var fromTop = $(this).scrollTop() + topMenuHeight;
 
-    // Get id of current scroll item
-    var cur = scrollItems.map(function() {
-      if ($(this).offset().top < fromTop)
-        return this;
-    });
-    // Get the id of the current element
-    cur = cur[cur.length - 1];
-    var id = cur && cur.length ? cur[0].id : "";
+  //   // Get id of current scroll item
+  //   var cur = scrollItems.map(function() {
+  //     if ($(this).offset().top < fromTop)
+  //       return this;
+  //   });
+  //   // Get the id of the current element
+  //   cur = cur[cur.length - 1];
+  //   var id = cur && cur.length ? cur[0].id : "";
 
-    if (lastId !== id) {
-      lastId = id;
-      // Set/remove active class
-      menuItems
-        .parent().removeClass("active")
-        .end().filter("[href=\\#" + id + "]").parent().addClass("active");
-    }
-  })
+  //   if (lastId !== id) {
+  //     lastId = id;
+  //     // Set/remove active class
+  //     menuItems
+  //       .parent().removeClass("active")
+  //       .end().filter("[href=\\#" + id + "]").parent().addClass("active");
+  //   }
+  // })
 
   /* ----------------------------------------------------------- */
   /*  10. HOVER DROPDOWN MENU
@@ -278,6 +278,6 @@ jQuery(function($) {
   /* ----------------------------------------------------------- */
   $(document).ready(function(event) {
     // initialize persistent state
-    jQuery('#aa-preloader-area').delay(300).fadeOut('slow'); // will fade out  
+    jQuery('#aa-preloader-area').delay(300).fadeOut('slow'); // will fade out
   });
 });
