3542b06ceee7c9aa9d1c055a1de444d49ab03adc
Nguyen Hoang Tu <nguyenhoangtu91@gmail.com>
2016-10-16 12:43:57 +0700

    Add average star

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -8,4 +8,24 @@ class FoodItem < ApplicationRecord
   def self.search(search)
     where("name LIKE ?", "%#{search}%")
   end
+
+  def sum_star
+    @total_star = 0
+    self.comments.each do |comment|
+      @total_star += comment.star
+    end
+    @total_star
+  end
+
+  def comment_number
+    self.comments.size
+  end
+
+  def average_star
+    if comment_number == 0
+      0
+    else
+      sum_star.to_f/comment_number
+    end
+  end
 end
